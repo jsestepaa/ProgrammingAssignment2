dbe603a3e53fe8a306ce7cfa4980596ed998424b
Sev <s.hawkeye03@gmail.com>
2020-05-12 15:53:32 -0500

    Final edits for the assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This code defines a new matrix class who has the capacity to store it's own inverse
+## in a cache, in case there's the need for it.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(baseMatrix = matrix()) {
+    baseInverse <- NULL
+    set <- function(newMatrix){
+        baseMatrix <<- newMatrix
+        baseInverse <<- NULL
+    }
+    get <- function(){
+        baseMatrix
+    }
+    setInverse <- function(newInverse){
+        baseInverse <<- newInverse
+    }
+    getInverse <- function(){
+        baseInverse
+    }
+    list(set = set, get = get,
+        setInverse = setInverse, 
+        getInverse = getInverse
+        )
 }
 
+## This function search for the inverse of the custom matrix. If it exist in the cache,
+## it calls it, but if the cached inverse is null, thefunction calculates it and saves
+## it in the cache.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(baseMatrix, ...) {
+    baseInverse <- baseMatrix$getInverse()
+    if(!is.null(baseInverse)) {
+        message("Getting cached data")
+        return(baseInverse)
+    }
+    temp <- baseMatrix$get()
+    baseInverse <- solve(temp)
+    baseMatrix$setInverse(baseInverse)
+    return(baseInverse)
 }
